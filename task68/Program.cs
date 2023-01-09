700d177596f80e4467d89be887f6bb450251c1ef
sofia <sonrud@mail.ru>
2023-01-09 17:10:32 +0300

    add 68

---
+++ task68/Program.cs
@@ -0,0 +1,15 @@
+﻿int rec(int a, int b)
+{
+    if(a==0)
+    return b+1;
+    else if(b==0)
+    return rec(a-1,1);
+   else return rec(a-1,rec(a,b-1));
+}
+
+Console.Clear();
+Console.WriteLine("Введите 1-ое число: ");
+int m=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n,m));
